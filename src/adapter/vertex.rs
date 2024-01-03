a3c9d50735d6fef2cf49d8320c17d7c20d0be41c
Kyle Anthony Williams <kyle.anthony.williams2@gmail.com>
2024-01-02 19:50:25 -0500

    Flesh out `Vector`, add helper methods

--- src/adapter/vertex.rs
+++ src/adapter/vertex.rs
@@ -1,17 +1,53 @@
-use rustc_hir::{ItemId, HirId};
+use rustc_hir::{ItemId, HirId, BodyId};
+use rustc_span::def_id::LocalDefId;
 
 #[non_exhaustive]
 #[derive(Debug, Clone, trustfall::provider::TrustfallEnumVertex)]
 pub enum Vertex {
-    Block(()),
-    Body(()),
+    Block(HirId),
+    Body(BodyId),
     Crate(()),
-    Expr(()),
-    Fn(()),
-    FnBody(()),
+    Expr(HirId),
+    Fn(ItemId),
+    FnBody(BodyId),
     Item(ItemId),
-    LocalStatement(()),
+    LocalStatement(HirId),
     Node(HirId),
-    Statement(()),
-    Ty(()),
+    Statement(HirId),
+    Ty(LocalDefId, HirId),
+}
+
+impl Vertex {
+    pub fn hir_id(&self) -> Option<HirId> {
+        if let Some(item_id) = self.clone().item_id() {
+            Some(item_id.hir_id())
+        } else if let Some(BodyId { hir_id }) = self.clone().body_id() {
+            Some(hir_id)
+        } else {
+            match self {
+                Self::Node(hir_id)
+                | Self::Block(hir_id)
+                | Self::Expr(hir_id)
+                | Self::Statement(hir_id)
+                | Self::LocalStatement(hir_id) => Some(*hir_id),
+                _ => None,
+            }
+        }
+    }
+
+    pub fn item_id(&self) -> Option<ItemId> {
+        match self {
+            Self::Item(item_id)
+            | Self::Fn(item_id) => Some(*item_id),
+            _ => None,
+        }
+    }
+
+    pub fn body_id(&self) -> Option<BodyId> {
+        match self {
+            Self::Body(body_id)
+            | Self::FnBody(body_id) => Some(*body_id),
+            _ => None,
+        }
+    }
 }
